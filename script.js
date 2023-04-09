0ff75f2901a81eb4bfd216ec4c0e38dc83a5c140
marjmekk <marjmekk1@gmail.com>
2023-04-08 22:43:45 -0700

    added new file

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-firstName = 'Marjorie'
-lastName = 'Mekkhavong'
-yearsOfStudy = '6 months'
-goal = 'have great knowledge with Advanced JavaScript'
+console.log("hello!")
+
+const firstName = 'Marjorie';
+const lastName = 'Mekkhavong';
+const yearsOfStudy = 1;
+const goal = 'become a full stack developer'
+
+//("My name is " + firstName + " " + lastName + ". I have been at SCC for " + yearsOfStudy + " years and plan to " + goal + " after completing my courses.");
+
+const introduction = "My name is ${firstName} ${lastName}. I have been at SCC for ${yearsOfStudy} and plan to ${goal} after completing my courses.";
+
+document.getElementById("introduction").innerHTML = introduction;
