ee19e684810af7bf4a3876aa73e459ddfb07320b
Marjorie <114688337+marjmekk@users.noreply.github.com>
2023-04-10 00:07:12 +0000

    added links

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-console.log("hello!")
-
-const firstName = 'Marjorie';
-const lastName = 'Mekkhavong';
-const yearsOfStudy = 1;
-const goal = 'become a full stack developer'
+let firstName = "Marjorie"
+let lastName = "Mekkhavong";
+let yearsOfStudy = 1;
+let  goal = 'become a full stack developer';
 
 //("My name is " + firstName + " " + lastName + ". I have been at SCC for " + yearsOfStudy + " years and plan to " + goal + " after completing my courses.");
 
-const introduction = "My name is ${firstName} ${lastName}. I have been at SCC for ${yearsOfStudy} and plan to ${goal} after completing my courses.";
+
+let introduction = 'My name is ${firstName} ${lastName}. I have been at SCC for ${yearsOfStudy} year and plan to ${goal} after completing my courses.'; 
 
 document.getElementById("introduction").innerHTML = introduction;
