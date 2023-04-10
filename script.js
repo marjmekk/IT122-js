f5ed21c7d892f0bc87cbbcc721b3bd27b94889bc
Marjorie <114688337+marjmekk@users.noreply.github.com>
2023-04-10 17:53:28 +0000

    fixed minor error

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let  goal = 'become a full stack developer';
 //("My name is " + firstName + " " + lastName + ". I have been at SCC for " + yearsOfStudy + " years and plan to " + goal + " after completing my courses.");
 
 
-let introduction = 'My name is ${firstName} ${lastName}. I have been at SCC for ${yearsOfStudy} year and plan to ${goal} after completing my courses.'; 
+let introduction = `My name is ${firstName} ${lastName}. I have been at SCC for ${yearsOfStudy} year and plan to ${goal} after completing my courses.`; 
+
+
 
 document.getElementById("introduction").innerHTML = introduction;
