0e43c0709f72cc72b1c64fd813d785816e16da8d
marjmekk <marjmekk1@gmail.com>
2023-04-14 17:07:09 -0700

    added new folder and files

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("hello!")
-
 const firstName = 'Marjorie';
 const lastName = 'Mekkhavong';
 const yearsOfStudy = 1;
@@ -7,6 +5,6 @@ const goal = 'become a full stack developer'
 
 //("My name is " + firstName + " " + lastName + ". I have been at SCC for " + yearsOfStudy + " years and plan to " + goal + " after completing my courses.");
 
-const introduction = "My name is ${firstName} ${lastName}. I have been at SCC for ${yearsOfStudy} and plan to ${goal} after completing my courses.";
+const introduction = "My name is ${firstName} ${lastName}.  I have been at SCC for ${yearsOfStudy} and plan to ${goal} after completing my courses.";
 
 document.getElementById("introduction").innerHTML = introduction;
